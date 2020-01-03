863aca1c8a1c671e20f49cd23782d477e55ca92e
wangzisong <dresong@126.com>
2020-01-03 17:31:44 +0800

    init files

---
+++ src/Http/Providers/DreJunitServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dresong\Http\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DreJunitSeriveProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+}
